e14df28b4bf72dab63384dad1894bf372f36336c
Joey Rizzoli <joey@lineageos.it>
2017-01-31 21:01:31 +0100

    Recorder: add latest records cards
    Allow the user to easily view, share and delete latest record
    
    Change-Id: I470576fddef2bfe14f830ba581d177d394519273
    Signed-off-by: Joey Rizzoli <joey@lineageos.it>

--- app/src/main/java/org/lineageos/recorder/sounds/SoundFragment.java
+++ app/src/main/java/org/lineageos/recorder/sounds/SoundFragment.java
@@ -19,23 +19,30 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import org.lineageos.recorder.R;
 import org.lineageos.recorder.RecorderActivity;
 import org.lineageos.recorder.ui.SoundVisualizer;
+import org.lineageos.recorder.utils.LastRecordHelper;
 import org.lineageos.recorder.utils.Utils;
 
 public class SoundFragment extends Fragment {
+    private static final String TYPE = "audio/wav";
 
     private Activity mActivity;
     private TextView mCardTitle;
     private SoundVisualizer mVisualizer;
     private Button mStopButton;
+    private CardView mLastCard;
+    private TextView mLastMessage;
 
     public SoundFragment() {
     }
@@ -50,8 +57,23 @@ public class SoundFragment extends Fragment {
         mCardTitle = (TextView) mView.findViewById(R.id.sound_recording_title);
         mVisualizer = (SoundVisualizer) mView.findViewById(R.id.sound_visualizer);
         mStopButton = (Button) mView.findViewById(R.id.sound_recording_button);
+        mLastCard = (CardView) mView.findViewById(R.id.sound_card_last);
+        mLastMessage = (TextView) mView.findViewById(R.id.sound_last_message);
+        ImageButton mPlayButton = (ImageButton) mView.findViewById(R.id.sound_last_play);
+        ImageButton mShareButton = (ImageButton) mView.findViewById(R.id.sound_last_share);
+        ImageButton mDeleteButton = (ImageButton) mView.findViewById(R.id.sound_last_delete);
+
         mStopButton.setOnClickListener(mButtonView ->
                 ((RecorderActivity) mActivity).toggleSoundRecorder());
+        mPlayButton.setOnClickListener(mButtonView -> startActivityForResult(
+                LastRecordHelper.getOpenIntent(getContext(), getFile(), TYPE), 0));
+        mShareButton.setOnClickListener(mButtonView ->
+                startActivity(LastRecordHelper.getShareIntent(getContext(), getFile(), TYPE)));
+        mDeleteButton.setOnClickListener(mButtonView -> {
+            AlertDialog mDialog = LastRecordHelper.deleteFile(getContext(), getFile(), true);
+            mDialog.setOnDismissListener(mListener -> refresh(mActivity));
+            mDialog.show();
+        });
 
         refresh(getContext());
         return mView;
@@ -75,6 +97,21 @@ public class SoundFragment extends Fragment {
         }
         mVisualizer.onAudioLevelUpdated(0);
         mStopButton.setVisibility(Utils.isSoundRecording(mContext) ? View.VISIBLE : View.GONE);
+        boolean hasLastRecord = getFile() != null;
+        mLastCard.setVisibility(hasLastRecord ? View.VISIBLE : View.GONE);
+        if (hasLastRecord) {
+            mLastMessage.setText(mActivity.getString(R.string.sound_last_message,
+                    LastRecordHelper.getLastItemDate(mActivity, true),
+                    LastRecordHelper.getLastItemDuration(mActivity, true) / 100));
+        }
+    }
+
+    private String getFile() {
+        if (mActivity == null) {
+            return null;
+        }
+
+        return LastRecordHelper.getLastItemPath(mActivity, true);
     }
 
 
