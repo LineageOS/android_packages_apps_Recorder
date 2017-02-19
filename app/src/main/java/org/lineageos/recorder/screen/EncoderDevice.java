16bff759330ad19fb995aa35dc892e7701052285
Aidas Jonikas <tcg96nougat@gmail.com>
2017-02-19 20:40:09 +0000

    EncoderDevice: Fix NPE (in some cases)
    * On my device, running run() causes it to catch an exception, but the Exception message is empty for some reason (and that causes a NPE with Recorder app crashing)
    
    Change-Id: Idd671d50c61af9677a49a64c3657b90397d0ee99

--- app/src/main/java/org/lineageos/recorder/screen/EncoderDevice.java
+++ app/src/main/java/org/lineageos/recorder/screen/EncoderDevice.java
@@ -317,7 +317,9 @@ abstract class EncoderDevice {
             try {
                 encode();
             } catch (Exception e) {
-                Log.e(LOGTAG, e.getMessage());
+                if (e.getMessage() != null) {
+                    Log.e(LOGTAG, e.getMessage());
+                }
             }
             cleanup();
             Log.i(LOGTAG, "=======ENCODING COMPLETE=======");
