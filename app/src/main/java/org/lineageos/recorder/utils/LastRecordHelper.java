a3d99bd93df4e787524deebc603bdba17b01f61d
2bllw8 <2bllw8@gmail.com>
2021-01-28 13:08:09 +0100

    Recorder: Add feature to rename recordings
    Change-Id: Ib3bf56c83146694b53b1075dbd7c0c2b44e13d7a

--- app/src/main/java/org/lineageos/recorder/utils/LastRecordHelper.java
+++ app/src/main/java/org/lineageos/recorder/utils/LastRecordHelper.java
@@ -20,12 +20,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.text.Editable;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.AppCompatEditText;
 
 import org.lineageos.recorder.DialogActivity;
 import org.lineageos.recorder.R;
 import org.lineageos.recorder.sounds.SoundRecorderService;
+
+import java.util.function.Consumer;
 
 public class LastRecordHelper {
     private static final String PREFS = "preferences";
@@ -71,6 +75,30 @@ public class LastRecordHelper {
                 .create();
     }
 
+    public static AlertDialog promptRename(Context context,
+                                           String currentTitle,
+                                           Consumer<String> consumer) {
+        AppCompatEditText editText = new AppCompatEditText(context);
+        editText.setSingleLine(true);
+        editText.setText(currentTitle);
+
+        return new AlertDialog.Builder(context)
+                .setTitle(R.string.list_edit_title)
+                .setView(editText)
+                .setPositiveButton(R.string.list_edit_confirm, (dialog, which) -> {
+                    Editable editable = editText.getText();
+                    if (editable == null || editable.length() == 0) {
+                        return;
+                    }
+
+                    String newTitle = editable.toString();
+                    if (!newTitle.equals(currentTitle)) {
+                        consumer.accept(newTitle);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .create();
+    }
 
     public static Intent getShareIntent(Uri uri, String mimeType) {
         Intent intent = new Intent(Intent.ACTION_SEND);
