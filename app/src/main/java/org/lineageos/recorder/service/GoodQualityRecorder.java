3d3e7674cde31e7d9511b867072af677a8185143
2bllw8 <2bllw8@gmail.com>
2021-02-08 10:37:46 +0100

    Recorder: Bring back original wav recording as an option
    I separate the two implementation behind an interface so it is possible to use
    the old code (that I optimized) for wav recording
    
    Signed-off-by: 2bllw8 <2bllw8@gmail.com>
    Change-Id: I382e02bc0653b684029fe6126fccc7da50145813

---
+++ app/src/main/java/org/lineageos/recorder/service/GoodQualityRecorder.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lineageos.recorder.service;
+
+import android.media.MediaRecorder;
+
+import java.io.File;
+import java.io.IOException;
+
+public class GoodQualityRecorder implements SoundRecording {
+    private static final String FILE_NAME_EXTENSION_OGG = "ogg";
+    private static final String FILE_MIME_TYPE_OGG = "audio/ogg";
+
+    private MediaRecorder mRecorder = null;
+    private boolean mIsPaused = false;
+
+    @Override
+    public void startRecording(File file) throws IOException {
+        mRecorder = new MediaRecorder();
+        mRecorder.setOutputFile(file);
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.OGG);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.OPUS);
+
+        mRecorder.prepare();
+        mRecorder.start();
+    }
+
+    @Override
+    public boolean stopRecording() {
+        if (mRecorder == null) {
+            return false;
+        }
+
+        if (mIsPaused) {
+            mIsPaused = false;
+            mRecorder.resume();
+        }
+
+        mRecorder.stop();
+        mRecorder.release();
+        return true;
+    }
+
+    @Override
+    public boolean pauseRecording() {
+        if (mIsPaused || mRecorder == null) {
+            return false;
+        }
+
+        mIsPaused = true;
+        mRecorder.pause();
+        return true;
+    }
+
+    @Override
+    public boolean resumeRecording() {
+        if (!mIsPaused || mRecorder == null) {
+            return false;
+        }
+
+        mRecorder.resume();
+        mIsPaused = false;
+        return true;
+    }
+
+    @Override
+    public int getCurrentAmplitude() {
+        return mRecorder == null ? 0 : mRecorder.getMaxAmplitude();
+    }
+
+    @Override
+    public String getMimeType() {
+        return FILE_MIME_TYPE_OGG;
+    }
+
+    @Override
+    public String getFileExtension() {
+        return FILE_NAME_EXTENSION_OGG;
+    }
+}
