e875fa02434a461d1c6dc2253c9f87ba41f9c4c4
Paul Keith <javelinanddart@gmail.com>
2021-02-13 14:51:38 -0600

    Recorder: Correct WAV header creation
    * This is supposed to be SampleRate * NumChannels * BitsPerSample/8
    * Source: http://soundfile.sapp.org/doc/WaveFormat/
    
    Change-Id: I579e4b6833044114a594b371167a6087f57c01a5

--- app/src/main/java/org/lineageos/recorder/sounds/PcmConverter.java
+++ app/src/main/java/org/lineageos/recorder/sounds/PcmConverter.java
@@ -30,7 +30,7 @@ class PcmConverter {
     private static final long SAMPLE_RATE = 44100;
     private static final int RECORDER_BPP = 16;
     private static final int CHANNELS = 1;
-    private static final long BYTE_RATE = RECORDER_BPP * 441000 * CHANNELS / 8;
+    private static final long BYTE_RATE = RECORDER_BPP * SAMPLE_RATE * CHANNELS / 8;
     private static final String TAG = "PcmConverter";
 
     @SuppressWarnings("SameParameterValue")
