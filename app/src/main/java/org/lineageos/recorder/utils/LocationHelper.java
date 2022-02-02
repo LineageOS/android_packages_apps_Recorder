e960a6014980adf032e9d269c61d0d8e4e675df8
2bllw8 <2bllw8@gmail.com>
2022-02-02 20:46:30 +0100

    Recorder: improve file name selection logic
    Change-Id: Ifedc6466455ac1eac34b8bff9a32beace7faf189

--- app/src/main/java/org/lineageos/recorder/utils/LocationHelper.java
+++ app/src/main/java/org/lineageos/recorder/utils/LocationHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2021 The LineageOS Project
+ * Copyright (C) 2020-2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Collections;
+import java.util.Optional;
 
 public final class LocationHelper {
     private static final String TAG = "LocationHelper";
@@ -45,42 +46,38 @@ public final class LocationHelper {
         preferences = new PreferencesManager(context);
     }
 
-    @Nullable
-    public String getCurrentLocationName() {
-        final Location lastGoodLocation = getLastGoodLocation();
-        if (lastGoodLocation == null) {
-            return null;
-        }
-
-        final Geocoder geocoder = new Geocoder(context);
-        try {
-            final List<Address> addressList = geocoder.getFromLocation(
-                    lastGoodLocation.getLatitude(), lastGoodLocation.getLongitude(), 1);
-            if (addressList.isEmpty()) {
-                return null;
-            }
-
-            final Address address = addressList.get(0);
-            final String featureName = address.getFeatureName();
-            // Street numbers may be returned here, ignore them
-            if (featureName != null && featureName.length() > 3) {
-                return featureName;
-            }
-            final String locality = address.getLocality();
-            if (locality != null) {
-                return locality;
-            }
-            return address.getCountryName();
-        } catch (IOException e) {
-            Log.e(TAG, "Failed to obtain address from last known location", e);
-        }
-        return null;
+    public Optional<String> getCurrentLocationName() {
+        return getLastGoodLocation()
+                .map(lastGoodLocation -> {
+                    final Geocoder geocoder = new Geocoder(context);
+                    try {
+                        return geocoder.getFromLocation(lastGoodLocation.getLatitude(),
+                                lastGoodLocation.getLongitude(), 1);
+                    } catch (IOException e) {
+                        Log.e(TAG, "Failed to obtain address from last known location", e);
+                        return Collections.<Address>emptyList();
+                    }
+                })
+                .filter(addressList -> !addressList.isEmpty())
+                .map(addressList -> addressList.get(0))
+                .map(address -> {
+                    final String featureName = address.getFeatureName();
+                    // Street numbers may be returned here, ignore them
+                    if (featureName != null && featureName.length() > 3) {
+                        return featureName;
+                    }
+                    final String locality = address.getLocality();
+                    if (locality == null) {
+                        return address.getCountryName();
+                    } else {
+                        return locality;
+                    }
+                });
     }
 
-    @Nullable
-    private Location getLastGoodLocation() {
+    private Optional<Location> getLastGoodLocation() {
         if (locationManager == null || !preferences.getTagWithLocation()) {
-            return null;
+            return Optional.empty();
         }
         Location lastGoodLocation;
         try {
@@ -96,12 +93,13 @@ public final class LocationHelper {
                         LocationManager.PASSIVE_PROVIDER);
             }
         } catch (SecurityException e) {
-            return null;
+            return Optional.empty();
         }
 
         if (lastGoodLocation == null || lastGoodLocation.getAccuracy() == 0f) {
-            return null;
+            return Optional.empty();
+        } else {
+            return Optional.of(lastGoodLocation);
         }
-        return lastGoodLocation;
     }
 }
