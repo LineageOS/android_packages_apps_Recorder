68fbffa77de23aec0d8255be5cf442d9def40cc9
luca020400 <luca.stefani.ge1@gmail.com>
2022-04-18 18:08:03 +0200

    Improve high quality recorder
    Record in stereo.
    
    This also makes the pcm converter
    configurable to allow easily playing
    with the options
    
    Change-Id: I33bcd5a8b2a925a06bc2e1a2693c62cdfcd872e5

--- app/src/main/java/org/lineageos/recorder/service/HighQualityRecorder.java
+++ app/src/main/java/org/lineageos/recorder/service/HighQualityRecorder.java
@@ -37,12 +37,13 @@ public class HighQualityRecorder implements SoundRecording {
     private static final String FILE_NAME_EXTENSION_WAV = "wav";
     private static final String FILE_MIME_TYPE_WAV = "audio/wav";
     private static final int SAMPLING_RATE = 44100;
-    private static final int CHANNEL_IN = AudioFormat.CHANNEL_IN_DEFAULT;
+    private static final int CHANNEL_IN = AudioFormat.CHANNEL_IN_STEREO;
     private static final int FORMAT = AudioFormat.ENCODING_PCM_16BIT;
     private static final int BUFFER_SIZE_IN_BYTES = 2 * AudioRecord.getMinBufferSize(SAMPLING_RATE,
             CHANNEL_IN, FORMAT);
 
     private AudioRecord mRecord;
+    private PcmConverter mPcmConverter;
     private File mFile;
     private int mMaxAmplitude;
 
@@ -54,8 +55,19 @@ public class HighQualityRecorder implements SoundRecording {
     @RequiresPermission(Manifest.permission.RECORD_AUDIO)
     public void startRecording(File file) {
         mFile = file;
-        mRecord = new AudioRecord(MediaRecorder.AudioSource.DEFAULT,
-                SAMPLING_RATE, CHANNEL_IN, FORMAT, BUFFER_SIZE_IN_BYTES);
+
+        AudioFormat audioFormat = new AudioFormat.Builder()
+                .setSampleRate(SAMPLING_RATE)
+                .setChannelMask(CHANNEL_IN)
+                .setEncoding(FORMAT)
+                .build();
+
+        mPcmConverter = new PcmConverter(audioFormat.getSampleRate(),
+                audioFormat.getChannelCount(),
+                audioFormat.getFrameSizeInBytes() * 8 / audioFormat.getChannelCount());
+
+        mRecord = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, audioFormat.getSampleRate(),
+                audioFormat.getChannelMask(), audioFormat.getEncoding(), BUFFER_SIZE_IN_BYTES);
         mRecord.startRecording();
 
         mIsRecording.set(true);
@@ -77,7 +89,7 @@ public class HighQualityRecorder implements SoundRecording {
         } catch (InterruptedException e) {
             // Wait at most 1 second, if we fail save the current data
         } finally {
-            PcmConverter.convertToWave(mFile, BUFFER_SIZE_IN_BYTES);
+            mPcmConverter.convertToWave(mFile, BUFFER_SIZE_IN_BYTES);
         }
 
         return true;
