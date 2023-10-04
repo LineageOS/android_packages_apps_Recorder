6a4851e9c05cefb3c301fcc552093d72efd772c0
Luca Stefani <luca.stefani.ge1@gmail.com>
2023-10-04 10:00:46 +0200

    Recorder: Write the actual buffer rather then expected size
    AudioRecord ensures the 'status' return value is the number of
    bytes read, while the buffer may contains unwanted data.
    
    Change-Id: Id456b1166c717735fbfb40488332fa8303962b24

--- app/src/main/java/org/lineageos/recorder/service/HighQualityRecorder.java
+++ app/src/main/java/org/lineageos/recorder/service/HighQualityRecorder.java
@@ -154,7 +154,7 @@ public class HighQualityRecorder implements SoundRecording {
                             // Status indicates the number of bytes
                             if (status != 0) {
                                 if (mTrackAmplitude.get()) {
-                                    for (int i = 0; i < data.length; i = i + 2) {
+                                    for (int i = 0; i < status; i = i + 2) {
                                         int value = data[i] & 0xff | data[i + 1] << 8;
                                         if (value < 0) {
                                             value = -value;
@@ -164,7 +164,7 @@ public class HighQualityRecorder implements SoundRecording {
                                         }
                                     }
                                 }
-                                out.write(data, 0, BUFFER_SIZE_IN_BYTES);
+                                out.write(data, 0, status);
                             }
                     }
                 } catch (IOException e) {
