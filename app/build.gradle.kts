a3d628a0eb08d3bca640a84ddb2e534503cfbfb9
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-09-21 11:24:31 +0200

    Recorder: Update kotlin deps to match AOSP 16
    Change-Id: I87a047c4d68c04886f8d1502b7aba6c9d1376e9d

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
 
 dependencies {
     // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.9.23"))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.1.10"))
 
     implementation("androidx.activity:activity-ktx:1.7.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
