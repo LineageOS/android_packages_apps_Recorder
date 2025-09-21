e462587f32033ab729628aa4aa249baed09254e8
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-09-21 11:53:44 +0200

    Recorder: Bump kotlin version
    Just to get gradle going
    
    Change-Id: I90f02613b5942900f2777290c914bce23a69c5f7

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.7.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("org.lineageos.generatebp") version "+" apply false
 }
 
